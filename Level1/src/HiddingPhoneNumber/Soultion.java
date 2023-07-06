125521f5389e3c98c265dcd1929c73f3c3826e3e
jin <jinhyun7675@gmail.com>
2023-07-06 16:46:37 +0900

    commitTest0706 16:45

---
+++ Level1/src/HiddingPhoneNumber/Soultion.java
@@ -0,0 +1,5 @@
+package HiddingPhoneNumber;
+
+public class Soultion {
+
+}
---
+++ Level1/src/HiddingPhoneNumber/SoultionRunner.java
@@ -0,0 +1,10 @@
+package HiddingPhoneNumber;
+
+public class SoultionRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
