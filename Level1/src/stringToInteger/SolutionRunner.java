f4ff15e1cea17fc291780f37f4bb667f2133b718
jin <jinhyun7675@gmail.com>
2023-07-07 21:21:42 +0900

    자릿수 더하기, 문자열을 정수형으로 바꾸기, 김서방 찾기 완료

---
+++ Level1/src/stringToInteger/SolutionRunner.java
@@ -0,0 +1,13 @@
+package stringToInteger;
+
+public class SolutionRunner {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		int s = sol.solution("-123");
+		
+		System.out.println(s);
+	}
+
+}
