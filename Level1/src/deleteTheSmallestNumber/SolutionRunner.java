e31df969174fd82a7f9206b219047f257fb1ca2e
jin <jinhyun7675@gmail.com>
2023-07-11 23:42:19 +0900

    제일 작은 수 제거하기 미완료

---
+++ Level1/src/deleteTheSmallestNumber/SolutionRunner.java
@@ -0,0 +1,15 @@
+package deleteTheSmallestNumber;
+
+public class SolutionRunner {
+
+	public static void main(String[] args) {
+
+		Solution sol = new Solution();
+		
+		int[] arr = {1,2,3,4};
+		int[] answer = sol.solution(arr);
+		
+		System.out.println(answer);
+	}
+
+}
