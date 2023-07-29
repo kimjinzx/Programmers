0c248089a8cd59e792f8b3296bd196095dd62c50
jin <jinhyun7675@gmail.com>
2023-07-29 16:24:34 +0900

    시허암호 문제완료

---
+++ Level1/src/caesarCipher/SolutionRunner.java
@@ -0,0 +1,13 @@
+package caesarCipher;
+
+public class SolutionRunner {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		String result = sol.solution("A B", 1);
+		
+		System.out.println(result);
+	}
+
+}
