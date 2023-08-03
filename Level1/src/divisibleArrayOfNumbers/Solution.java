47fde01d8357cab2a5ad7acdfe5b9fcf9a456bd3
jin <jinhyun7675@gmail.com>
2023-08-04 01:54:41 +0900

    문제 풀이 과정 추가

--- Level1/src/divisibleArrayOfNumbers/Solution.java
+++ Level1/src/divisibleArrayOfNumbers/Solution.java
@@ -1,7 +1,5 @@
 package divisibleArrayOfNumbers;
 
-import java.util.Arrays;
-
 //문제 설명
 //array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
 //divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
@@ -25,34 +23,37 @@ import java.util.Arrays;
 //입출력 예#3
 //3, 2, 6은 10으로 나누어 떨어지지 않습니다. 나누어 떨어지는 원소가 없으므로 [-1]을 리턴합니다.
 
+import java.util.Arrays;
+
 public class Solution {
-	 public int[] solution(int[] arr, int divisor) {
+	   public int[] solution(int[] arr, int divisor) {
 	        
-	        int count = 0;
-	        int number = 0;
+	        int count = 0; // 약수를 반환한 횟수 
+	        int index = 0;
 	        
-	        for(int i = 0; i < arr.length; i++){
-	            if(arr[i] % divisor == 0){
-	                count++;
-	            }
+	        for(int i = 0; i<arr.length; i++) { // 배열원소값들을 조회하기 위한 반복문 
+	        	if(arr[i] % divisor == 0) { // arr의 원소들이 divisor에 약수인지 판단하는 조건문 
+	        		count++; // 약수가 나올때마다 카운트됨 
+	        	}
 	        }
 	        
-	        if(count == 0){
-	            int[] answer = {-1};
-	            return answer;
+	        if(count == 0) { // count가 0일 경우 약수로 반환되는 원소는 없다는 조건문 
+	        	int answer[] = {-1}; // 그러므로 -1 리턴 
+	        	return answer; 
 	        }
 	        
-	        int[] answer = new int[count];
+	        int answer[] = new int[count];  // 배열 초기화 
 	        
-	        for(int i = 0; i < arr.length; i++){
-	            if(arr[i] % divisor == 0){
-	                answer[number] = arr[i];
-	                number++;
-	            }
+	        // 약수들을 answer[]의 원소로 넣는 작업 
+	        for(int i = 0; i<arr.length; i++) { 
+	        	if(arr[i] % divisor == 0) {
+	        		answer[index] += arr[i];
+	        		index++; // 다음 배열 원소추가를 위한 인덱스의 증분식 
+	        	}
 	        }
 	        
-	        Arrays.sort(answer);
+	        Arrays.sort(answer); // 오름차순으로 정렬 
+	        
 	        return answer;
-	        
 	    }
 }
