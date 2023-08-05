bae9c35c38ae9cbbf3ad6cd95ef11cf4833f7e5b
jin <jinhyun7675@gmail.com>
2023-08-05 23:41:11 +0900

    정수 제곱근 판별 문제완료

---
+++ Level1/src/IntegerSquareRootDetermination/SolutionRunner.java
@@ -0,0 +1,14 @@
+package IntegerSquareRootDetermination;
+
+public class SolutionRunner {
+
+	public static void main(String[] args) {
+		
+		Solution sol = new Solution();
+		
+		long result = sol.solution(144);
+		
+		System.out.println(result);
+	}
+
+}
