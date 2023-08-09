95ae3e7055e7bc71790ee3db3f2f98a8283ae3e4
jin <jinhyun7675@gmail.com>
2023-08-09 23:42:55 +0900

    가운데 글자 가져오기 문제완료

---
+++ Level1/src/getMiddleLetter/SolutionRunner.java
@@ -0,0 +1,14 @@
+package getMiddleLetter;
+
+public class SolutionRunner {
+
+	public static void main(String[] args) {
+		
+		Solution sol = new Solution();
+		
+		String result = sol.solution("abcdef");
+	
+		System.out.println(result);
+	}
+
+}
