89d2f04a7ea911ada16124743d4150a38efc156e
jin <jinhyun7675@gmail.com>
2023-08-13 01:05:34 +0900

    Eclipse Commit Test 2

--- Level1/src/createMinimum/Solution.java
+++ Level1/src/createMinimum/Solution.java
@@ -24,7 +24,7 @@ package createMinimum;
 //입출력 예 설명
 //A에서 첫번째 숫자인 1, B에서 두번째 숫자인 4를 뽑아 곱하여 더합니다. 
 //(누적된 값 : 4) 다음, A에서 두번째 숫자인 2, B에서 첫번째 숫자인 3을 뽑아 곱하여 더합니다. (누적된 값 : 4 + 6 = 10)
-//이 경우가 최소이므로 10을 return 합니다...
+//이 경우가 최소이므로 10을 return 합니다
 
 import java.util.Arrays;
 //import java.util.Collections;
