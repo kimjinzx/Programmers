e3255276ef421440e56f94df6b3b54ceab4971cb
jin <jinhyun7675@gmail.com>
2023-08-13 16:44:41 +0900

    숫자의 표현 문제완료

---
+++ Level1/src/representationOfNumbers/SolutionRunner.java
@@ -0,0 +1,12 @@
+package representationOfNumbers;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        int num = sol.solution(15);
+
+        System.out.println(num);
+    }
+}
