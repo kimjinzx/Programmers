abb3262e9103829fbb051a2af58517f28fcbbf22
jin <jinhyun7675@gmail.com>
2023-08-24 17:54:52 +0900

    예상 대진표 문제완료

---
+++ Level1/src/expectedBracket/SolutionRunner.java
@@ -0,0 +1,10 @@
+package expectedBracket;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+        int expected = sol.solution(8, 4, 7);
+        System.out.println(expected);
+    }
+}
